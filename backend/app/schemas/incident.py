53d1970ca5bcc31897208c254971fbde70f13d7f
danial_rube <dnlnorozi@gmail.com>
2026-08-30 00:13:33 +0330

    update(bakcend): add CRITICAL incidnet severity level.

--- backend/app/schemas/incident.py
+++ backend/app/schemas/incident.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict, Field
 from enum import Enum
 from  datetime import datetime
 
@@ -12,6 +12,7 @@ class SeverityLevel(str, Enum):
     LOW = "low"
     MEDIUM = "medium"
     HIGH = "high"
+    CRITICAL = "critical"
 
 class IncidentBase(BaseModel):
     title_fa: str
@@ -21,13 +22,33 @@ class IncidentBase(BaseModel):
 
 #------Output/Input Incident Schema-----
 class IncidentCreate(IncidentBase):
-    matched_ticket_ids: list[int]
+    #Guide: اسکیمایی که توسط سرویس-رخداد استفاده میشه
+    matched_ticket_ids: list[int] = Field(default_factory=list)
+    #avg_similarity_score = float | None = None
+
+class IncidentUpdate(BaseModel):
+    """
+    اسکیمایی که برای آپدیت وضعیت، شدت یا افزودن تیکت جدید به رخداد موجود استفاده می‌شود.
+    تمام فیلدها اختیاری هستند تا فقط موارد لازم آپدیت شوند (Partial Update).
+    """
+    title_fa: str | None = None
+    reason_fa: str | None = None
+    severity: SeverityLevel | None = None
+    status: IncidentStatus | None = None
+    new_ticket_ids: list[int] | None = None
+    resolved_at: datetime | None = None
+    #avg_similarity_score: Optional[float] = None
 
 class IncidentResponse(IncidentBase):
+    #Guid: اسکیمای نهایی ارسال شده به فرانت
     id: int
-    matched_ticket_ids: list[int]
+    ticket_count: int
+    matched_ticket_ids: list[int] = Field(default_factory=list)
     created_at: datetime
     updated_at: datetime
+    resolved_at: datetime | None = None
 
-    class Config:
-        from_attributes = True
+    # avg_similarity_score: Optional[float] = None
+
+    # این تنظیم جایگزین class Config: orm_mode = True در Pydantic v2 است
+    model_config = ConfigDict(from_attributes=True)
